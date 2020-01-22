c73aceeed9223ffb4a99ae257f699187911e7bd7
Stephen Rivera <st.rivera_94@yahoo.com>
2020-01-22 17:10:09 -0500

    commiting to sync with front end

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,15 @@ class User < ApplicationRecord
     has_many :disciplines
     has_many :media, through: :disciplines
 
-    has_many :projects
-    has_many :collaborations
+    has_many :projects, dependent: :destroy
+    has_many :collaborations, dependent: :destroy
     has_many :joined_projects, through: :collaborations, source: :project
 
     # has_many :reviews
 
-    has_many :reviews_as_reviewer, class_name: 'Review', foreign_key: :reviewer_id
+    has_many :reviews_as_reviewer, class_name: 'Review', foreign_key: :reviewer_id, dependent: :destroy
     has_many :reviewees, through: :reviews_as_reviewer #, source: :reviewee
 
-    has_many :reviews_as_reviewee, class_name: 'Review', foreign_key: :reviewee_id
+    has_many :reviews_as_reviewee, class_name: 'Review', foreign_key: :reviewee_id, dependent: :destroy
     has_many :reviewers, through: :reviews_as_reviewee #, source: :reviewer
 end
